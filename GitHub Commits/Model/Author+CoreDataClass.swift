9f2ef14ff3f488b7b523a4f4cff9fc504781fb1d
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-15 14:36:17 +0300

    data objects are generated again

---
+++ GitHub Commits/Model/Author+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Author+CoreDataClass.swift
+//  GitHub Commits
+//
+//  Created by Evgeniy Ryshkov on 15/01/2019.
+//  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Author)
+public class Author: NSManagedObject {
+
+}
--- GitHub Commits/Model/Commit+CoreDataProperties.swift
+++ GitHub Commits/Model/Commit+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Commit+CoreDataProperties.swift
 //  GitHub Commits
 //
-//  Created by Evgeniy Ryshkov on 14/01/2019.
+//  Created by Evgeniy Ryshkov on 15/01/2019.
 //  Copyright © 2019 Evgeniy Ryshkov. All rights reserved.
 //
 //
@@ -21,5 +21,6 @@ extension Commit {
     @NSManaged public var message: String
     @NSManaged public var sha: String
     @NSManaged public var url: String
+    @NSManaged public var author: Author
 
 }
