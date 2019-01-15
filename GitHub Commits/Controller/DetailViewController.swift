8b9cbc3abac85a20c0d8d267cf79cb0d3405fd00
Evgeniy Ryshkov <eryshkov@gmail.com>
2019-01-15 15:00:04 +0300

    DetailViewController is changed

--- GitHub Commits/Controller/DetailViewController.swift
+++ GitHub Commits/Controller/DetailViewController.swift
@@ -13,10 +13,21 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailLabel: UILabel!
     
     //MARK: -
+    var detailItem: Commit?
+    
+    //MARK: -
+    @objc func showAuthorCommits() {
+        
+    }
+    
+    //MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let detail = self.detailItem {
+            detailLabel.text = detail.message
+            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Commit 1/\(detail.author.commits.count)", style: .plain, target: self, action: #selector(showAuthorCommits))
+        }
     }
     
 
